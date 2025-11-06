67fc66ddab92397d4f42e3093178b47ec867b16b
Cruz <lindsayanne.cruz@benilde.edu.ph>
2025-11-06 09:50:03 +0800

    Collected participant info (Task 2)

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,7 +1,18 @@
 print("Welcome to SMIT TechFest!\nEvent organized by Lindsay Cruz of APPDAET BTCS1")
-print("How many participants will register?")
-num_participants = int(input())
 
 try:
+    num_participants = int(input("How many participants will register?: "))
     if num_participants <= 0: print("Invalid number of participants"), exit()
 except ValueError: print("Invalid number of participants"), exit()
+
+participants = [] # list for participants
+
+for i in range(num_participants):
+    name = input(f"Enter participant name: ")
+    track = input(f"Enter track: ")
+    dict_participant = {"name": name, "track": track} # dict for participants
+    participants.append(dict_participant)
+
+print("\nRegistered Participants:")
+for regis in participants:
+    print(f'{regis["name"]} - {regis["track"]}')
